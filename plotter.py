eefad87fa0bee306aff0231ed9b1f07270d06bb2
Mateus Vieira <habdig7@gmail.com>
2024-10-06 15:35:49 -0300

    ok

--- plotter.py
+++ plotter.py
@@ -21,9 +21,28 @@ def plot_all(arquivos_configuracao: list[dict[str]], outdir: str):
     for grafico in arquivos_configuracao:
         print(grafico)
         figura, ax = plotlib.subplots()
-        ax.plot([1,2], [3,4])
+
+        for i, legendas in zip(grafico["pontos"],grafico["legendas"]): 
+            x = []
+            y = []
+            for j in i:
+                v1, v2 = eval(j)
+                x.append(v1)
+                y.append(v2)
+            print(legendas)
+            print(x)
+            print(y)
+            print("--")
+            ax.plot(x,y, label=legendas)  
+ 
+        
+        if grafico["grid"] != None:
+            ax.grid(color=grafico["grid"]["cor"], linestyle = grafico["grid"]["estilo"], linewidth = grafico["grid"]["tamanho"])
 
         ax.set_title(grafico["titulo"])
+        ax.set_xlabel(grafico["xlabel"])
+        ax.set_ylabel(grafico["ylabel"])
+        ax.legend()
         plotlib.savefig(f"{outdir}/{grafico['nome_arquivo']}.{grafico['formato']}")
         plotlib.show()
 
